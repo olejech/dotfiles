6a16f815612174c09be78d54f066392465c95d89
olejech <olejech@gmail.com>
2023-12-16 00:04:42 +0300

    feat(fish): add gruvbox theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting ""
 set PATH /usr/local/bin $PATH
 
+theme_gruvbox dark hard
+
 source ~/dotfiles/fish/config.local.fish
 
 alias lazygit="lazygit --use-config-file=$LAZY_GIT_LOCAL_CONFIG_PATH"
