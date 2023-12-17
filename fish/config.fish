cc0f1d3dd9a86441c64f9dfacfc84625d4acd053
olejech <olejech@gmail.com>
2023-12-17 13:22:58 +0300

    feat(fish): enable vi-mode

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,9 @@ set PATH /usr/local/bin $PATH
 
 theme_gruvbox dark hard
 
+# Enable vi-mode
+fish_vi_key_bindings
+
 source ~/dotfiles/fish/config.local.fish
 
 alias lazygit="lazygit --use-config-file=$LAZY_GIT_LOCAL_CONFIG_PATH"
