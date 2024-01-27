c75dc3cc62e749026feb79c6dd724cb62a2d4888
olejech <olejech@gmail.com>
2024-01-27 19:01:41 +0300

    feat(fish): add alias zettelkasten

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,4 @@ fish_vi_key_bindings
 source ~/dotfiles/fish/config.local.fish
 
 alias lazygit="lazygit --use-config-file=$HOME/dotfiles/lazygit/config.yml,$LAZY_GIT_LOCAL_CONFIG_PATH"
+alias zk="nvim $ZETTELKASTEN_PATH"
