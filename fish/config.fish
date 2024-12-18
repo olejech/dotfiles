57676df9149b1ba5096e225f7aec779768ee7f4f
olejech <olejech@gmail.com>
2024-12-18 23:17:49 +0700

    feat(fish): add zoxide

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,5 @@ source ~/dotfiles/fish/config.local.fish
 
 alias lazygit="lazygit --use-config-file=$HOME/dotfiles/lazygit/config.yml,$LAZY_GIT_LOCAL_CONFIG_PATH"
 alias zk="nvim $ZETTELKASTEN_PATH"
+
+zoxide init fish | source
