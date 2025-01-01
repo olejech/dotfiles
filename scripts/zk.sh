1e2d4c974e1e7dd1c09310d04a2dec52f3b3895d
olejech <olejech@gmail.com>
2025-01-01 16:50:40 +0700

    fix(scripts): run zk if not running yet

--- scripts/zk.sh
+++ scripts/zk.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 session_name="zk"
 
-# 1. First you check if a tmux session exists with a given name.
-tmux has-session -t=$session_name 2> /dev/null
-
-# 2. Create the session if it doesn't exist.
-if [[ $? -ne 0 ]]; then
+if ! tmux has-session -t="$session_name" 2>/dev/null; then
   TMUX='' tmux new-session -d -s "$session_name"
 fi
 
-tmux switch-client -t "$session_name" \; send-keys -t "$session_name" "cd '$ZETTELKASTEN_PATH'; nvim" C-m
-tmux send-keys -t "$session_name" ":FzfLua files cwd=Base" C-m
+tmux switch-client -t "$session_name"
+
+if ! tmux list-panes -t "$session_name" -F "#{pane_current_command}" | grep -q "nvim"; then
+  tmux send-keys -t "$session_name" "cd '$ZETTELKASTEN_PATH'; nvim" C-m
+fi
+
+tmux send-keys -t "$session_name" ":ObsidianQuickSwitch" C-m
