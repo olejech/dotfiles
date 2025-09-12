e1ee5d74667ffa4e3b0a65b1817f02df7c1201ca
olejech <olejech@gmail.com>
2025-09-12 15:53:13 +0300

    fix(fish): theme

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,8 @@ zoxide init fish | source
 
 # uv
 fish_add_path "$HOME/.local/bin"
+
+function fish_prompt
+    printf '%s%s%s> ' \
+                    (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
