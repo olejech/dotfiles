b03f4560d494f4edc3a5e0f217bffaa569071501
olejech <olejech@gmail.com>
2025-09-12 15:50:21 +0300

    fix(hammerspoon): use Zen browser

--- .hammerspoon/app-launcher.lua
+++ .hammerspoon/app-launcher.lua
@@ -1,12 +1,12 @@
 local modifiers = { "cmd", "ctrl" }
 hs.hotkey.bind(modifiers, "B", function()
-  hs.application.launchOrFocus("Arc")
+	hs.application.launchOrFocus("Zen")
 end)
 
 hs.hotkey.bind(modifiers, "E", function()
-  hs.application.launchOrFocus("Alacritty")
+	hs.application.launchOrFocus("Alacritty")
 end)
 
 hs.hotkey.bind(modifiers, "C", function()
-  hs.application.launchOrFocus("Calendar")
+	hs.application.launchOrFocus("Calendar")
 end)
