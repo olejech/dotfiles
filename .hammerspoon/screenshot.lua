92772a54f810d7fbb8ccf2e21cae120f1e480fb2
olejech <olejech@gmail.com>
2025-10-09 22:13:27 +0300

    updaate(mappings): change mappings

--- .hammerspoon/screenshot.lua
+++ .hammerspoon/screenshot.lua
@@ -1,8 +1,8 @@
-hs.hotkey.bind({ "cmd", "shift" }, "4", function()
+hs.hotkey.bind({ "cmd", "ctrl" }, "4", function()
 	hs.eventtap.keyStroke({ "cmd", "shift", "ctrl" }, "4")
 end)
 
-hs.hotkey.bind({ "cmd", "shift" }, "2", function()
+hs.hotkey.bind({ "cmd", "ctrl" }, "2", function()
 	local file = os.date("~/Screenshots/screenshot-%Y%m%d-%H%M%S.png")
 	hs.execute("screencapture -i " .. file)
 	hs.execute("open -a Preview " .. file)
